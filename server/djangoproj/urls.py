6e90d7f8c1ad54ff63f0c63d06d4a27a2b6b6f34
Pedro Gabriel Prepelec Demarco <117846752+Gabrieldemarco@users.noreply.github.com>
2025-02-16 19:35:35 -0300

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-#from djangoapp import views
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic import TemplateView
