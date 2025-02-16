cbc2de96e8ae8a72663ee219b35e086042027841
Pedro Gabriel Prepelec Demarco <117846752+Gabrieldemarco@users.noreply.github.com>
2025-02-16 19:31:06 -0300

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,8 +29,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://pedroprepele-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.'
-    'cognitiveclass.ai/',
+    'https://pedroprepele-8000.theianext-1-labs-prod-misc-tools-us-east-0.'
+    'proxy.cognitiveclass.ai/',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
